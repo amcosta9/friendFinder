30c68b31fccb18533420e7075b635dbc2353f640
Ariel <ariel.m.little@gmail.com>
2017-04-22 23:53:52 -0400

    survey and results calculating

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,22 @@
 /**
  * Created by Ariel on 4/22/2017.
  */
-var friendsArr = [];
+var friendsArr = [
+    {
+        name: 'Bubbles',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vr3jsZDDL.jpg',
+        scores: [ '3', '3', '3' ]
+    },
+    {
+        name: 'Buttercup',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41te4jIxCKL.jpg',
+        scores: [ '2', '2', '2' ]
+    },
+    {
+        name: 'Blossom',
+        photo: 'https://static.tumblr.com/e98605d23f1ac709fd908f1b5c0666b0/318gax9/RiXoa7ze3/tumblr_static_5ui1ujrcd4w04g8g8oosgc8gg.jpg',
+        scores: [ '1', '1', '1' ]
+    }
+];
 
 module.exports = friendsArr;
