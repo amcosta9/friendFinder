e571392e83dddd60f3d44b3c63d078e90ee75f9e
Ariel <ariel.m.little@gmail.com>
2017-04-24 23:53:51 -0400

    increased questions, added to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,17 +5,17 @@ var friendsArr = [
     {
         name: 'Bubbles',
         photo: 'https://images-na.ssl-images-amazon.com/images/I/41vr3jsZDDL.jpg',
-        scores: [ '3', '3', '3' ]
+        scores: [ '3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
     },
     {
         name: 'Buttercup',
         photo: 'https://images-na.ssl-images-amazon.com/images/I/41te4jIxCKL.jpg',
-        scores: [ '2', '2', '2' ]
+        scores: [ '2', '2', '2', '2', '2', '2', '2', '2', '2', '2']
     },
     {
         name: 'Blossom',
         photo: 'https://static.tumblr.com/e98605d23f1ac709fd908f1b5c0666b0/318gax9/RiXoa7ze3/tumblr_static_5ui1ujrcd4w04g8g8oosgc8gg.jpg',
-        scores: [ '1', '1', '1' ]
+        scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
     }
 ];
 
